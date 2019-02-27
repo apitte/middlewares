c39a5d340fcacf58bd73f230602f07bc06c26070
Marek Bartos <bartos.developer152@gmail.com>
2019-02-27 17:38:15 +0100

    Store uri in RequestScopeStorage

--- src/ApiMiddleware.php
+++ src/ApiMiddleware.php
@@ -3,6 +3,7 @@
 namespace Apitte\Middlewares;
 
 use Apitte\Core\Dispatcher\IDispatcher;
+use Apitte\Core\Http\RequestScopeStorage;
 use Contributte\Middlewares\IMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -13,15 +14,23 @@ class ApiMiddleware implements IMiddleware
 	/** @var IDispatcher */
 	protected $dispatcher;
 
-	public function __construct(IDispatcher $dispatcher)
+	/** @var RequestScopeStorage */
+	private $requestScopeStorage;
+
+	public function __construct(IDispatcher $dispatcher, RequestScopeStorage $requestScopeStorage)
 	{
 		$this->dispatcher = $dispatcher;
+		$this->requestScopeStorage = $requestScopeStorage;
 	}
 
 	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
 	{
+		$this->requestScopeStorage->save('uri', $request->getUri());
+
 		// Pass this API request/response objects to API dispatcher
 		$response = $this->dispatcher->dispatch($request, $response);
+		
+		$this->requestScopeStorage->clear();
 
 		// Pass response to next middleware
 		$response = $next($request, $response);
